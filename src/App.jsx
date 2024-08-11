8c9c7c260e09ccfac41b5a6f0652b1d9a90127ef
Vikram Jha <jhavikram11121998@gmail.com>
2024-08-11 11:38:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, {createContext} from "react"
+
+// import Components
+import Navbar from "./components/Navbar"
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Call from "./components/Call";
+import Features from "./components/Features";
+import Clients from "./components/Clients";
+import Stats from "./components/Stats";
+import Pricing from "./components/Pricing";
+import Questions from "./components/Questions";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+
+//let's create a context;
+const themeContext = createContext(); 
+
+
+function App() {
+  const theme = {
+    themeColor: "#009961",
+    themeColorHover: "#01784c",
+    lightBgColor: "#F9F9F9",
+  }
+  return (
+    <>
+    <themeContext.Provider value={theme}>
+      <Navbar></Navbar>
+      <Home></Home>
+      <About></About>
+      <Services></Services>
+      <Call></Call>
+      <Features></Features>
+      <Clients></Clients>
+      <Stats></Stats>
+      <Pricing></Pricing>
+      <Questions></Questions>
+      <Contact></Contact>
+      <Footer></Footer>
+    </themeContext.Provider>
+    </>
+  )
+}
+
+export default App
+export {themeContext};
